c647987ebaec3611d81300c03c67b1f0c41cb5b2
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-07-06 21:41:36 +0530

    Create LoveLetter.js

---
+++ LoveLetter.js
@@ -0,0 +1,28 @@
+export function LoveLetter() {
+  return (
+    <div className="card">
+      <h2>💌 Love Letter</h2>
+      <p>
+        My dearest love,<br /><br />
+        Vanii
+Jai Laxmi Apartment
+Near Patparganj
+Delhi – 110092
+Date: 05 June 2025
+Arjun Gupta
+Geeta Apartment, 17-block
+Geeta Colony
+Delhi – 110031
+Subject: Just wanted to say what I feel
+Hey my Vanii,
+You are my most beautiful person of my life and I love you endlessly from bottom of my heart. Your each words are like order for me but I follow those who are right for me hehe. Talking with you whole day make me feel happy. Being angry on you is thing I don’t like to do. I always want to bring all happiness and all things you deserve in this life and keep you like my princess and give you princess treatment each and every day. I love to do work given by you to me and I just do it with happiness.
+I. am always here for you and I Love you a lot
+Yours dumboo,
+Aru
+05-06-25.<br /><br />
+        With all my heart,<br />
+        ❤️ Yours forever
+      </p>
+    </div>
+  );
+}
