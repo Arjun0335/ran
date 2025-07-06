fda0d6c5f862de7c6df757646ef16ea62f3875de
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-07-06 21:38:53 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { Gallery } from './Gallery';
+import { LoveLetter } from './LoveLetter';
+import { Music } from './Music';
+import { Countdown } from './Countdown';
+import { Quotes } from './Quotes';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>🌹 To My Love 💖</h1>
+        <p>Every heartbeat of mine speaks your name...</p>
+      </header>
+      <Countdown />
+      <LoveLetter />
+      <Gallery />
+      <Music />
+      <Quotes />
+      <footer className="footer">
+        Made with ❤️ by [Your Name]
+      </footer>
+    </div>
+  );
+}
+
+export default App;
